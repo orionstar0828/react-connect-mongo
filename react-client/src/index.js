2e2443ad4a172f8c5be38d39bedc59c396c8b84c
orionstar0828 <orionstar0828@gmail.com>
2022-04-13 04:48:07 +0200

    react connect mongodb

---
+++ react-client/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/reduxStore";
+
+import "./index.css";
+import App from "./App";
+
+let isAuthorized = false;
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
